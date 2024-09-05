7a956a86d9a4c4fd27417471119153e59adc47ca
Satyam <157725917+satyaBoss1@users.noreply.github.com>
2024-09-04 20:51:53 -1100

    Update java.js

--- java.js
+++ java.js
@@ -11,7 +11,7 @@ arr.forEach(button =>{
         }
         else if(e.target.innerHTML=='Ac'){
             string='';
-            input.value='';
+            input.value=string;
         }
         else if(e.target.innerHTML=='DEL'){
             string=string.substring(0,string.length-1);
